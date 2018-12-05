dbeea3d9e9ce4c8e5fe35d091c2e59132c104d68
Lucas Oliveira <lucasabreuoliveira@gmail.com>
2018-12-05 08:01:26 +0100

    Work done in 28-11-18

---
+++ src/components/ProductComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="product">
+    <h1>{{ product.title }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Product } from '@/types/Product';
+
+@Component
+export default class ProductComponent extends Vue {
+  @Prop() private product!: Product;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/ProductList.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul class="product-list">
+    <li v-for="product in products" v-bind:key="product.id">
+      <ProductComponent v-bind:product="product"/>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import ProductComponent from '@/components/ProductComponent.vue'; // @ is an alias to /src
+
+const products = [
+  {
+    id: '1',
+    title: 'Hat',
+    description: 'Nice hat',
+  },
+  {
+    id: '2',
+    title: 'Scarf',
+    description: 'Red scarf',
+  },
+];
+
+@Component({
+  components: {
+    ProductComponent,
+  },
+})
+export default class ProductList extends Vue {
+  private data() {
+    return { products };
+  }
+}
+</script>
